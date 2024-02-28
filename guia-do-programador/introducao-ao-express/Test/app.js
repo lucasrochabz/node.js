7d0184b2118fae3bcb736713bda4d16cd368c25a
Lucas Rocha <lucasbezerrar@gmail.com>
2024-02-28 18:30:50 -0300

    Add aulas de Introducao ao express, parametros e rotas

---
+++ guia-do-programador/introducao-ao-express/Test/app.js
@@ -0,0 +1,3 @@
+// npm -v => Para verificar se o npm está instalado
+// npm install express --save => Para instalar o Express
+// é bom criar um arquivo .gitignore para evitar que arquivos desnecessários sejam incluídos  no repositório
