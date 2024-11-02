9ee29cdb71246dba35f045adf84d1288cfc2e860
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-02 19:48:09 -0300

    retirando rota

--- guia-do-programador/aula07-rotas/Test/index.js
+++ guia-do-programador/aula07-rotas/Test/index.js
@@ -13,10 +13,6 @@ app.get('/blog', function (req, res) {
   res.send('Bem-vindo ao meu blog!');
 });
 
-app.get('/meuamor', function (req, res) {
-  res.send('<h1>Eu te amo!</h1>');
-});
-
 app.listen(8081, function () {
   console.log('Servidor Rodando na url http://localhost:8081');
 });
