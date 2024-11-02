3c62a0fed475f5e7198a47b15fc0afdcff211054
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-02 19:47:23 -0300

    pequenos ajustes

--- guia-do-programador/aula07-rotas/Test/index.js
+++ guia-do-programador/aula07-rotas/Test/index.js
@@ -1,18 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Seja bem-vindo ao meu app!')
-})
+app.get('/', function (req, res) {
+  res.send('Seja bem-vindo ao meu app!');
+});
 
-app.get('/sobre', function(req, res) {
+app.get('/sobre', function (req, res) {
   res.send('Minha pagina sobre');
-})
+});
 
-app.get('/blog', function(req, res) {
-  res.send('Bem-vindo ao meu blog!')
-})
+app.get('/blog', function (req, res) {
+  res.send('Bem-vindo ao meu blog!');
+});
 
-app.listen(8081, function() {
-  console.log('Servidor Rodando na url http://localhost:8081')
+app.get('/meuamor', function (req, res) {
+  res.send('<h1>Eu te amo!</h1>');
+});
+
+app.listen(8081, function () {
+  console.log('Servidor Rodando na url http://localhost:8081');
 });
